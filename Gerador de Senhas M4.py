7a4f1c5f36449e188c2893250a742bd2792fdb5b
razeraheloisa <razeraheloisa58@gmail.com>
2026-01-19 10:44:54 +0000

    Modifiquei o meu código
    Added email validation and password generation based on user input.

--- Gerador de Senhas M4.py
+++ Gerador de Senhas M4.py
@@ -2,26 +2,28 @@ import random
 
 print("=== CRIAR CONTA NO APLICATIVO ===")
 
-# Dados do usuário
 nome = input("Digite seu nome: ")
+
+
 email = input("Digite seu e-mail: ")
+while "@" not in email:
+    print("E-mail inválido. O e-mail precisa conter @")
+    email = input("Digite seu e-mail novamente: ")
+
 usuario = input("Digite um nome de usuário: ")
 
-# Listas de letras para senha fácil
-consoantes = "bcdfghjklmnpqrstvwxyz"
-vogais = "aeiou"
 
-senha = ""
+palavra = input("Digite uma palavra para gerar sua senha: ")
 
-# Gera uma senha fácil de decorar (5 letras)
-for i in range(5):
-    if i % 2 == 0:
-        senha += random.choice(consoantes)
-    else:
-        senha += random.choice(vogais)
+numeros = "0123456789"
 
-# Primeira letra maiúscula
-senha = senha.capitalize()
+senha = palavra.lower()
+
+senha = senha[0].upper() + senha[1:]
+
+
+for i in range(3):
+    senha += random.choice(numeros)
 
 print("\n=== CONTA CRIADA COM SUCESSO ===")
 print("Nome:", nome)
